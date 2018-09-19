8b7edd0b783d70da4942a3a5cc9ef17f3ae8cf21
Inho Lee <Inho@Inhos-MacBook-Pro.local>
2018-09-18 19:48:12 -0700

    First commit

---
+++ Makestagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Makestagram
+//
+//  Created by Inho Lee on 9/18/18.
+//  Copyright © 2018 Inho Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
